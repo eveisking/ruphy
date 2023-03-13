6d58382c6985c0cd49e1d0c8161a0fab5e39bcc6
eveisking <kingeve0202@gmail.com>
2023-03-13 23:55:04 +0100

    vite installation and assets

---
+++ ruphy/src/App.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import './App.css'
+import Home from './components/Home'
+
+function App() {
+  return (
+    <div className="App">
+      
+      <div className="card">
+      <Home />
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
+export default App
