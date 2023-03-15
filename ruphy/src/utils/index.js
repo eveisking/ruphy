7da545f194c009c6fbdaa2c59ba809e541cd7d73
eveisking <kingeve0202@gmail.com>
2023-03-15 16:36:33 +0100

    added data for generating random post

---
+++ ruphy/src/utils/index.js
@@ -0,0 +1,11 @@
+import { surpriseMePrompts } from "../constants";
+
+export function getRandomPrompts(prompt){
+    const randomIndex = Math.floor(Math.random() * 
+    surpriseMePrompts.length);
+    const randomPrompts = surpriseMePrompts[randomIndex];
+
+    if(randomPrompts === prompt ) return getRandomPrompts(prompt);
+
+    return randomPrompts;
+}
