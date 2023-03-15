71227aacd03fc42926c04fedb21f91e3f016371c
eveisking <kingeve0202@gmail.com>
2023-03-15 16:33:20 +0100

    routing with react router dom

--- ruphy/src/App.jsx
+++ ruphy/src/App.jsx
@@ -3,12 +3,21 @@ import './App.css'
 import {BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 
 import {logo} from './assets'
+import { Home, CreatePost } from './pages'
+import { Header } from './components'
 
 function App() {
   return (
     <div className="App">
-      <h1 className='text-3xl font-bold underline'>Hello World</h1>
-      
+     <BrowserRouter>
+      <Header></Header>
+      <main className='w-full sm:p-8 px-4 py-8 bg-[#f9fafe] min-h-[calc(100vh-73px)] mx-auto'>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='create-post' element={<CreatePost/>}/>
+        </Routes>
+      </main>
+     </BrowserRouter>      
     </div>
   )
 }
