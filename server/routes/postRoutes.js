32c5a7581d995d596e84c3dbf793b53813a7149e
eveisking <kingeve0202@gmail.com>
2023-03-16 01:40:47 +0100

    created post route and integrated dalle api

---
+++ server/routes/postRoutes.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import { v2 as cloudinary } from 'cloudinary';
+
+import Post from '../mongodb/models/post.js';
+dotenv.config();
+
+const router = express.Router();
+
+router.post('/', (req, res) => {
+    
+    
+});
+
+export default router;
