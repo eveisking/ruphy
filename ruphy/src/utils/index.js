25b01f6227e46fdf198ac73998e156d78dfb322b
eveisking <kingeve0202@gmail.com>
2023-03-25 06:11:32 +0100

    added function for  downloading image

--- ruphy/src/utils/index.js
+++ ruphy/src/utils/index.js
@@ -1,3 +1,5 @@
+import FileSaver from 'file-saver';
+
 import { surpriseMePrompts } from "../constants";
 
 export function getRandomPrompts(prompt){
@@ -8,4 +10,8 @@ export function getRandomPrompts(prompt){
     if(randomPrompts === prompt ) return getRandomPrompts(prompt);
 
     return randomPrompts;
+}
+
+export async function downloadImage(_id, photo) {
+FileSaver.saveAs(photo, `download-${_id}.jpg`);
 }
