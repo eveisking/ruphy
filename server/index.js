30721224bed2358390814535c769cf8ba32e20f8
eveisking <kingeve0202@gmail.com>
2023-03-25 06:08:00 +0100

    added bodyparser middleware

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 
 import connectDB from './mongodb/connect.js';
 import postRoutes from './routes/postRoutes.js';
@@ -9,7 +10,10 @@ import ruphyRoutes from './routes/ruphyRoutes.js';
 dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb'}));
 
 app.use(cors());
 app.use(express.json({ limit: '50mb'}));
@@ -18,7 +22,7 @@ app.use('/api/v1/post', postRoutes);
 app.use('/api/v1/ruphy', ruphyRoutes);
 
 app.get('/', async (req, res)=> {
- res.send('Hello from Ruphy!');
+ res.send('Hello from Ruph!');
 })
 
 const startServer = async () => {
